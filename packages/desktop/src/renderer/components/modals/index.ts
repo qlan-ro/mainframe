060e0b4a91f0e24dc2eed8fa34f179e08783192b
Doru Chiulan <doru.chiulan@gmail.com>
2026-05-05 19:41:37 +0300

    feat: add modals index exports

---
+++ packages/desktop/src/renderer/components/modals/index.ts
@@ -0,0 +1,5 @@
+export { ReviewPanel } from './ReviewPanel';
+export { ReviewPanelHeader } from './ReviewPanelHeader';
+export { FileTree } from './FileTree';
+export { DiffView } from './DiffView';
+export { ActionBar } from './ActionBar';
